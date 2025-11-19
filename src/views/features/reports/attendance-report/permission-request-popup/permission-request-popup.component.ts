4bbd541edd3fe9553b7b0716caca418a2eae9034
kareem-shehab <Kareem.Shehab@3pillarstc.com>
2025-11-19 16:10:44 +0200

    work done

---
+++ src/views/features/reports/attendance-report/permission-request-popup/permission-request-popup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permission-request-popup',
+  imports: [],
+  templateUrl: './permission-request-popup.component.html',
+  styleUrl: './permission-request-popup.component.scss'
+})
+export class PermissionRequestPopupComponent {
+
+}
