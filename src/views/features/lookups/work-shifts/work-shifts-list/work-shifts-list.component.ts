d41711812d51c67f3ebf3fb6083ae4dedab62830
Mahmoud Soliman <mahmoud.soliman@3pillars-eg.com>
2025-11-23 12:22:33 +0200

    add shift type filter to work-shifts-list filters

--- src/views/features/lookups/work-shifts/work-shifts-list/work-shifts-list.component.ts
+++ src/views/features/lookups/work-shifts/work-shifts-list/work-shifts-list.component.ts
@@ -41,6 +41,10 @@ export default class WorkShiftsListComponent
   extends BaseListComponent<Shift, WorkShiftsListPopupComponent, ShiftService, ShiftsFilter>
   implements OnInit
 {
+  shiftTypes = [
+    { label: this.translateService.instant('WORK_SHIFTS.FLEXIBLE'), value: true },
+    { label: this.translateService.instant('WORK_SHIFTS.FIXED'), value: false }
+  ];
   filterModel: ShiftsFilter = new ShiftsFilter();
 
   shiftService = inject(ShiftService);
@@ -58,10 +62,21 @@ export default class WorkShiftsListComponent
     return this.shiftService;
   }
 
-  override initListComponent(): void {}
+  override initListComponent(): void {
+    this.changeShiftTypesTranslation();
+  }
 
   protected override getBreadcrumbKeys() {
     return [{ labelKey: 'WORK_SHIFTS.WORK_SHIFTS' }];
+  }
+
+  changeShiftTypesTranslation() {
+    this.languageService.languageChanged$.subscribe((_) => {
+      this.shiftTypes = [
+        { ...{ label: this.translateService.instant('WORK_SHIFTS.FLEXIBLE'), value: true } },
+        { ...{ label: this.translateService.instant('WORK_SHIFTS.FIXED'), value: false } },
+      ];
+    });
   }
 
   openDialog(model: Shift): void {
@@ -121,9 +136,5 @@ export default class WorkShiftsListComponent
       if (model.isActive === false) return this.translateService.instant('WORK_SHIFTS.NOT_ACTIVE');
       else return '---';
     }
-  }
-
-  getShiftType() {
-
   }
 }
