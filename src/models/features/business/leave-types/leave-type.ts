8a4729a17c388adebe6e6a6a5105b4acae892f0b
Youssef <youssef.hassan@3pillars-eg.com>
2026-01-25 10:23:50 +0200

    Make availableTimesDuringServicePeriod required

--- src/models/features/business/leave-types/leave-type.ts
+++ src/models/features/business/leave-types/leave-type.ts
@@ -80,7 +80,7 @@ export class LeaveType extends BaseCrudModel<LeaveType, LeaveTypeService> {
       hasAnnualBalance: [hasAnnualBalance ?? true, [Validators.required]],
       annualBalance: [
         annualBalance,
-        [ 
+        [
           Validators.min(CustomValidators.defaultLengths.ANNUAL_BALANCE_MIN),
           Validators.max(CustomValidators.defaultLengths.ANNUAL_BALANCE_MAX),
         ],
@@ -92,6 +92,7 @@ export class LeaveType extends BaseCrudModel<LeaveType, LeaveTypeService> {
       availableTimesDuringServicePeriod: [
         availableTimesDuringServicePeriod,
         [
+          Validators.required,
           Validators.min(CustomValidators.defaultLengths.AVAILABLE_TIMES_DURING_SERVICE_PERIOD_MIN),
           Validators.max(CustomValidators.defaultLengths.AVAILABLE_TIMES_DURING_SERVICE_PERIOD_MAX),
         ],
