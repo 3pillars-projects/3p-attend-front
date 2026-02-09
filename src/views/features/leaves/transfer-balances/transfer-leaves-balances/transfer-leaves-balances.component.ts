0b730d3ce0d17eb706ef53e9f6b2a4505aa315cd
Youssef <youssef.hassan@3pillars-eg.com>
2026-02-09 10:51:27 +0200

    UI modification for transferring leave balances.

--- src/views/features/leaves/transfer-balances/transfer-leaves-balances/transfer-leaves-balances.component.ts
+++ src/views/features/leaves/transfer-balances/transfer-leaves-balances/transfer-leaves-balances.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { InputNumber } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
-import { Paginator, PaginatorState } from 'primeng/paginator';
 import { Select } from 'primeng/select';
 import { TableModule } from 'primeng/table';
 @Component({
   selector: 'app-transfer-leaves-balances',
-  imports: [TableModule, Paginator, Select, BreadcrumbModule, InputTextModule],
+  imports: [TableModule, Select, BreadcrumbModule, InputTextModule, InputNumber],
   templateUrl: './transfer-leaves-balances.component.html',
   styleUrl: './transfer-leaves-balances.component.scss',
 })
@@ -15,14 +15,9 @@ export default class TransferLeavesBalancesComponent {
   date2: Date | undefined;
   attendance!: any[];
   items: MenuItem[] | undefined;
-  first: number = 0;
   home: MenuItem | undefined;
-  rows: number = 10;
 
-  onPageChange(event: PaginatorState) {
-    this.first = event.first ?? 0;
-    this.rows = event.rows ?? 10;
-  }
+
   ngOnInit() {
     this.items = [{ label: 'لوحة المعلومات' }, { label: 'قائمة الفعاليات' }];
     this.attendance = [
