c5045e965b5fb9e3e05dcf4c53ea1f94266c5f7e
Youssef <youssef.hassan@3pillars-eg.com>
2026-03-04 11:18:41 +0200

    update TransferBalanceRequest

--- src/models/features/business/leaves-balances/transfer-balance-request.ts
+++ src/models/features/business/leaves-balances/transfer-balance-request.ts
@@ -1,7 +1,7 @@
-import { BalanceOperationType } from '@/enums/balance-operation-type-enum';
+import { LeaveTransferActionType } from '@/enums/leave-transfer-action-type';
 
 export class TransferBalanceRequest {
-  actionType!: BalanceOperationType; // Maps to operation type (1=Forward, 2=Backward, 3=Add)
+  actionType!: LeaveTransferActionType; // Maps to operation type (1=Forward, 2=Backward, 3=Add)
   departmentId?: number; // Filter (optional)
   leaveTypeId!: number; // Required filter
   transferAmount?: number; // Number of days to transfer/add
