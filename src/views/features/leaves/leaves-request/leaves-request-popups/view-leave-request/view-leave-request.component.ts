f28716858f06147b1eb74ddffc9d83fa91cac95d
Mohamed Hosam <mohamed.hosam@3pillars-eg.com>
2026-06-11 13:46:48 +0300

    cancellation and cut leave requests

--- src/views/features/leaves/leaves-request/leaves-request-popups/view-leave-request/view-leave-request.component.ts
+++ src/views/features/leaves/leaves-request/leaves-request-popups/view-leave-request/view-leave-request.component.ts
@@ -204,16 +204,19 @@ export class ViewLeaveRequestComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe((result) => {
-      if (result == DIALOG_ENUM.OK) {
-        this.alertService.showSuccessMessage({
-          messages: ['LEAVE_REQUEST_PAGE.CUT_LEAVE_SUCCESS'],
-        });
-      } else {
-        this.alertService.showErrorMessage({ messages: ['LEAVE_REQUEST_PAGE.CUT_LEAVE_FAILED'] });
-      }
-    });
-    this.model.cutLeave().subscribe(() => {
-      this.dialogRef.close(DIALOG_ENUM.OK);
+      if (result !== DIALOG_ENUM.OK) return;
+
+      this.model.cutLeave().subscribe({
+        next: () => {
+          this.alertService.showSuccessMessage({
+            messages: ['LEAVE_REQUEST_PAGE.CUT_LEAVE_SUCCESS'],
+          });
+          this.dialogRef.close(DIALOG_ENUM.OK);
+        },
+        error: () => {
+          this.alertService.showErrorMessage({ messages: ['LEAVE_REQUEST_PAGE.CUT_LEAVE_FAILED'] });
+        },
+      });
     });
   }
 }
